0002a5855192ed29e8df76e7ffee9e36e757bf36
AlejandroZBarajas <falejandrozb@gmail.com>
2025-03-04 22:52:56 -0600

    rutas

--- src/core/infrastructureC/router.go
+++ src/core/infrastructureC/router.go
@@ -1,16 +1,14 @@
 package infrastructurec
 
-import "net/http"
-
-/* import (
-	"MINI-MULTI-API-PRODUCER/src/events/infrastructure"
-) */
+import (
+	"net/http"
+)
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Origin", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Origin", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Methos", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
@@ -32,9 +30,15 @@ func MethodHandler(w http.ResponseWriter, r *http.Request) {
 
 	case http.MethodDelete:
 		ec.DeleteHandler(w, r)
+
+	default:
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
 	}
 }
 
-func SetRoutes() {
+func SetRoutes(ec *Infrastructure.EventController) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/events", MethodHandler)
+	http.Handle("/", corsMiddleware(mux))
 
 }
